e0422d456553e36cb21d95a80f7e879762739c5a
Miguel Alejandro Santiago Perez <miguelsantiagoews@gmail.com>
2022-09-21 06:50:20 -0500

    Working joystick

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovement _playerInput = default;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField]  private float gravityValue = -9.81f;
+
+    void Awake()
+    {
+        _playerInput = new PlayerMovement();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movementInput = _playerInput.PlayerMain.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if (_playerInput.PlayerMain.Jump.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
