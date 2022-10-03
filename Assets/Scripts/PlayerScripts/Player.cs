7ebc004f4eb33be9067ad6a160506ab50784978e
Miguel Alejandro Santiago Perez <miguelsantiagoews@gmail.com>
2022-10-02 21:43:47 -0500

    healthbarcreated and scripts

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject _gameOver = default;
     [SerializeField] private GameObject _pauseButton = default;
+    [SerializeField] private PlayerHealthBar _healthBar = default;
+    [SerializeField] private int _healthLevel = 10;
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _currentHealth;
 
     void Start()
     {
-        
+        _maxHealth = SetMaxHealth();
+        _currentHealth = _maxHealth;
+        _healthBar.SetMaxHealth(_maxHealth);
     }
 
     void Update()
@@ -19,5 +25,16 @@ public class Player : MonoBehaviour
             _gameOver.SetActive(true);
             _pauseButton.SetActive(false);
         }
+    }
+
+    private int SetMaxHealth()
+    {
+        _maxHealth = _healthLevel * 10;
+        return _maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth = _currentHealth - damage;
     }
 }
