fa3b900430957a1ccf4e20516f929174c81e8b54
Miguel Alejandro Santiago Perez <85505816+MikeLordx@users.noreply.github.com>
2022-11-06 23:25:07 -0600

    UI buttons edited

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,9 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.OnScreen;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject _lightAttackButton;
+    public GameObject _deactivatedLightAttackButton;
+    public GameObject _heavyAttackButton;
+    public GameObject _deactivatedHeavyAttackButton;
+    public GameObject _dashButton;
+    public GameObject _deactivatedDashButton;
+    public PlayerController _controller;
+
+    private void Update()
+    {
+        DeactivateLightAttack();
+        DeactivateHeavyAttack();
+    }
+
+    private void DeactivateLightAttack()
+    {
+        if (_controller._lightAttack)
+        {
+            StartCoroutine(DeactivateLigthAttackTimer());
+        }
+    }
     
+    private void DeactivateHeavyAttack()
+    {
+        if (_controller._heavyAttack)
+        {
+            StartCoroutine(DeactivateHeavyAttackTimer());
+        }
+    }
+
+    IEnumerator DeactivateLigthAttackTimer()
+    {
+        _lightAttackButton.SetActive(false);
+        _deactivatedLightAttackButton.SetActive(true);
+        yield return new WaitForSeconds(0.8f);
+        _lightAttackButton.SetActive(true);
+        _deactivatedLightAttackButton.SetActive(false);
+    }
+    
+
+    IEnumerator DeactivateHeavyAttackTimer()
+    {
+        _heavyAttackButton.SetActive(false);
+        _deactivatedHeavyAttackButton.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        _heavyAttackButton.SetActive(true);
+        _deactivatedHeavyAttackButton.SetActive(false);
+    }
 }
