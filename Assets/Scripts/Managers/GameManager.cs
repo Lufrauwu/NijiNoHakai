478a2c8130dd234f43e396a8562152ad1b3e9689
Lufrauwu <lexius002@gmail.com>
2022-11-23 11:27:35 -0600

    NewScene

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance = default;
-    public Vector3 _lastCheckpointPos = default;
+    public Vector3 _lastCheckpointPos = new Vector3(610, 8.75f, 295);
+    //[SerializeField] private GameObject _Player = default;
 
     private void Awake()
     {
+        
         if (_instance == null)
         {
             _instance = this;
@@ -17,6 +19,13 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+       
+    }
+
+    private void Start()
+    {
+        GameObject.FindGameObjectWithTag("Player").transform.position = _lastCheckpointPos;
     }
 
     private void Update()
---
+++ Assets/Scripts/PlayerScripts/PlayerRespawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+   /* private float _posX, _posY, _posZ;
+    
+    void Start()
+    {
+        
+    }
+
+    public void ReachedCheckpoit(float x, float y, float z)
+    {
+        PlayerPrefs.SetFloat();
+    }
+*/
+}
